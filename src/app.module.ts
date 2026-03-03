990a85eabe22770778a4a9f71149775ccb4db216
jcerda1 <jcerda.dev@gmail.com>
2026-03-03 17:54:33 -0600

    feat: Add scaffold for items, ledger and purchases

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ItemsModule } from './items/items.module';
+import { LedgerModule } from './ledger/ledger.module';
+import { PurchasesModule } from './purchases/purchases.module';
 
 @Module({
-  imports: [],
+  imports: [ItemsModule, LedgerModule, PurchasesModule],
   controllers: [AppController],
   providers: [AppService],
 })
