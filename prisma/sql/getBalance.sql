364e632782b3a4ba9486e0ef26d41ab1f8dac340
jcerda1 <jcerda.dev@gmail.com>
2026-03-04 17:34:52 -0600

    Fix(feat): Fix sql lock for concurrency

--- prisma/sql/getBalance.sql
+++ prisma/sql/getBalance.sql
@@ -1,8 +1,14 @@
 -- @param {String} $1:userId
+WITH _lock AS (
+  SELECT pg_advisory_xact_lock(('x' || substr(md5($1), 1, 16))::bit(64)::bigint)
+)
 SELECT COALESCE(SUM(
   CASE WHEN type = 'CREDIT' THEN amount
        WHEN type = 'DEBIT'  THEN -amount
   END
 ), 0) AS balance
-FROM ledger_transactions
-WHERE user_id = $1;
+FROM (
+  SELECT * FROM ledger_transactions
+  WHERE user_id = $1
+  FOR UPDATE
+) AS lt;
