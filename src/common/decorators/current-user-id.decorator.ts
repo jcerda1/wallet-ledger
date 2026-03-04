08cfe8e337233db903e3db34807cbce2608e1213
jcerda1 <jcerda.dev@gmail.com>
2026-03-04 16:01:08 -0600

    feat: Add current user decorator

---
+++ src/common/decorators/current-user-id.decorator.ts
@@ -0,0 +1,8 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const CurrentUserId = createParamDecorator(
+  (_data: unknown, ctx: ExecutionContext): string | undefined => {
+    const req = ctx.switchToHttp().getRequest();
+    return req.headers['x-user-id'] as string | undefined;
+  },
+);
