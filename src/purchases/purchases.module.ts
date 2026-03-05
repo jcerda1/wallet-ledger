4bcfb7144fa55507ecb3df6a98941afe73d816f0
jcerda1 <jcerda.dev@gmail.com>
2026-03-05 00:42:37 -0600

    Fix(ci): Update itemsService imports

--- src/purchases/purchases.module.ts
+++ src/purchases/purchases.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PurchasesService } from './purchases.service';
 import { PurchasesController } from './purchases.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { ItemsModule } from 'src/items/items.module';
 
 @Module({
+  imports: [PrismaModule, ItemsModule],
   providers: [PurchasesService],
   controllers: [PurchasesController],
 })
